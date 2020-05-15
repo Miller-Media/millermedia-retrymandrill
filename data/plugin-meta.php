b5875e46983c7b0758e5eef2bdeef963c8f6e48f
Fatimah Naqib <fatimah.naqib@miller-media.com>
2020-05-14 17:37:11 -0700

    new build

--- data/plugin-meta.php
+++ data/plugin-meta.php
@@ -5,7 +5,7 @@ return <<<'JSON'
     "namespace": "MillerMedia\\RetryMandrill",
     "name": "Retry Emails (Send Emails with Mandrill extension)",
     "slug": "millermedia-retrymandrill",
-    "version": "1.0.2",
+    "version": "1.1.0",
     "description": "Queues up failed Mandrill API calls by queuing",
     "author": "Miller Media",
     "author_url": "http:\/\/www.millermedia.io"
